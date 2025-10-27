fdbb1b5aa30aca3483f93e6f0e5bd56eac99889a
Aditya Jain <adityaj8@illinois.edu>
2025-10-27 14:46:27 -0500

    vioblock intf

--- sys/dev/vioblk.c
+++ sys/dev/vioblk.c
@@ -41,6 +41,31 @@
 // INTERNAL CONSTANT DEFINITIONS
 //
 
+//VIOBLK device structure
+
+struct vioblk_storage {
+    struct storage base;
+    volatile struct virtio_mmio_regs * regs;
+    int irqno;
+    char opened;
+
+    struct virtq_desc *desc;
+    struct virtq_avail *avail;
+    struct virtq_used *used;
+    unsigned int queue_size;
+    uint16_t next_desc_idx;
+
+    struct {
+        uint32_t type;
+        uint32_t reserved;
+        uint64_t sector;
+    } req_header;
+    uint8_t req_status;
+
+    struct condition ready;
+    struct lock lock;
+};
+
 // VirtIO block device feature bits (number, *not* mask)
 
 #define VIRTIO_BLK_F_SIZE_MAX 1
@@ -55,6 +80,17 @@
 #define VIRTIO_BLK_F_DISCARD 13
 #define VIRTIO_BLK_F_WRITE_ZEROES 14
 
+// INTERNAL GLOBAL INTERFACES 
+
+static const struct storage_intf vioblk_storage_intf = {
+    .blksz = 512,
+    .open = &vioblk_storage_open,
+    .close = &vioblk_storage_close,
+    .fetch = &vioblk_storage_fetch,
+    .store = &vioblk_storage_store,
+    .cntl = &vioblk_storage_cntl
+};
+
 // INTERNAL FUNCTION DECLARATIONS
 //
 
@@ -67,6 +103,7 @@
  * then return -EBUSY.
  */
 static int vioblk_storage_open(struct storage* sto);
+
 
 /**
  * @brief Resets the virtq avail and virtq used queues and sets necessary flags in vioblk device. If
@@ -140,7 +177,7 @@ static void vioblk_isr(int irqno, void* aux);
  */
 void vioblk_attach(volatile struct virtio_mmio_regs* regs, int irqno) {
     virtio_featset_t enabled_features, wanted_features, needed_features;
-    struct vioblk_storage* vbd;
+    struct vioblk_storage* blk;
     unsigned int blksz;
     int result;
 
@@ -183,17 +220,81 @@ void vioblk_attach(volatile struct virtio_mmio_regs* regs, int irqno) {
     // blksz must be a power of two
     assert(((blksz - 1) & blksz) == 0);
 
-    // FIXME
+    // The driver will typically initialize the virtual queue in the following way:
+
+    // Select the queue writing its index (first queue is 0) to QueueSel.
+    // Check if the queue is not already in use: read QueueReady, and expect a returned value of zero (0x0).
+    // Read maximum queue size (number of elements) from QueueNumMax. If the returned value is zero (0x0) the queue is not available.
+    // Allocate and zero the queue memory, making sure the memory is physically contiguous.
+    // Notify the device about the queue size by writing the size to QueueNum.
+    // Write physical addresses of the queue’s Descriptor Area, Driver Area and Device Area to (respectively) the QueueDescLow/QueueDescHigh, QueueDriverLow/QueueDriverHigh and QueueDeviceLow/QueueDeviceHigh register pairs.
+    // Write 0x1 to QueueReady.
+    blk = kcalloc(1, sizeof(*blk));
+    blk->regs = regs;
+    blk->irqno = irqno;
+    blk->opened = 0;
+    condition_init(&blk->ready, "vioblk.ready");
+    lock_init(&blk->lock);
+
+
+    regs->queue_sel = 0;
+    blk->queue_size = 32;
+    blk->next_desc_idx = 0;
+    
+    uintptr_t ptr;
+    ptr = (uintptr_t)kcalloc(16 * blk->queue_size + 15, 1);
+    blk->desc = (void*)((ptr + 15) & ~15);
+
+    ptr = (uintptr_t)kcalloc(6 + 2 * blk->queue_size + 1, 1);
+    blk->avail = (void*)((ptr + 1) & ~1);
+
+    ptr = (uintptr_t)kcalloc(6 + 8 * blk->queue_size + 3, 1);
+    blk->used = (void*)((ptr + 3) & ~3);
+
+    regs->queue_num = blk->queue_size;
+    virtio_attach_virtq(regs, 0, blk->queue_size, (uint64_t)blk->desc, (uint64_t)blk->used, (uint64_t)blk->avail);
+
+    struct storage_intf* intf = kmalloc(sizeof(*intf));
+    *intf = vioblk_storage_intf;
+    intf->blksz = blksz;
+
+    long long capacity = (long long)regs->config.blk.capacity * 512;
+    storage_init(&blk->base, intf, capacity);
+
+    register_device(VIOBLK_NAME, DEV_STORAGE, &blk->base);
+
+    regs->status |= VIRTIO_STAT_DRIVER_OK;
 }
 
 static int vioblk_storage_open(struct storage* sto) {
-    // FIXME
-    return -ENOTSUP;
+    if (sto == NULL) return -EINVAL;
+    struct vioblk_storage * const blk =
+        (void*)sto - offsetof(struct vioblk_storage, base);
+
+    if (blk->opened) return -EBUSY;
+
+    //figure out what device stuff we need to do
+    blk->opened = 1;
+    enable_intr_source(blk->irqno, VIORNG_INTR_PRIO, &vioblk_isr, blk);
+    virtio_enable_virtq(blk->regs, 0);
+    // FIXME your code goes here
+    return 0;
 }
 
 static void vioblk_storage_close(struct storage* sto) {
-    // FIXME
-    return;
+    if (sto == NULL) return;
+    struct vioblk_storage * const blk =
+        (void*)sto - offsetof(struct vioblk_storage, base);
+
+    if (!blk->opened) return;
+    blk->opened = 0;
+    disable_intr_source(blk->irqno); //disable in plic
+    virtio_reset_virtq(blk->regs, 0); //reset virtq, dunno what it sets lowks
+
+    // kfree((void*)blk->desc);
+    // kfree((void*)blk->avail);
+    // kfree((void*)blk->used);
+    // kfree((void*)sto->intf);
 }
 
 static long vioblk_storage_fetch(struct storage* sto, unsigned long long pos, void* buf,
@@ -210,10 +311,21 @@ static long vioblk_storage_store(struct storage* sto, unsigned long long pos, co
 
 static int vioblk_storage_cntl(struct storage* sto, int op, void* arg) {
     // FIXME
-    return -ENOTSUP;
+    struct vioblk_storage * const blk = (void*)sto - offsetof(struct vioblk_storage, base);
+
+    if (op == FCNTL_GETEND) {
+        if (arg == NULL) return -EINVAL;
+        *(unsigned long long*)arg = blk->base.capacity;
+        return 0;
+    }
+    else {
+            return -ENOTSUP;
+    }
 }
 
 static void vioblk_isr(int irqno, void* aux) {
-    // FIXME
-    return;
+    struct vioblk_storage * const blk = aux;
+    blk->regs->interrupt_ack = blk->regs->interrupt_status;
+
+    condition_broadcast(&blk->ready);
 }
