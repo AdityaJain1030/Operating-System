939c8164fd7675dcc3228dcbbc304db582816bbe
Alex Lin <alexlin3@illinois.edu>
2025-11-13 16:25:32 -0600

    basic memory done. Trying to add the reset_smpace and still figuring out if we need to allocate space to phys mem or not

--- sys/memory.c
+++ sys/memory.c
@@ -55,6 +55,7 @@
 //
 
 // linker-provided (kernel.ld)
+// kimg = kernel image
 extern char _kimg_start[];
 extern char _kimg_text_start[];
 extern char _kimg_text_end[];
@@ -82,28 +83,84 @@ char memory_initialized = 0;
  * linked list of chunks. Initially, all free pages are in a single large chunk. To
  * allocate a block of pages, we break up the smallest chunk in the list
  */
+/*
+-Goals/Idea: Free list of page chunks should be sorted by physical addresses so that way when freeing it is easy to merge
+
+
+tracks physical pages I believe?
+
+
+
+*/
 struct page_chunk {
     struct page_chunk *next;  ///< Next page in list
     unsigned long pagecnt;    ///< Number of pages in chunk
+    uint64_t      address;      // start address of page_chunk
 };
 
 /**
  * @brief RISC-V PTE. RTDC (RISC-V docs) for what each of these fields means!
+ * 
+ * ^ So useless. :/
+ * 
+ * Unhelpful commnet use this iNSTEAD: https://docs.riscv.org/reference/isa/_attachments/riscv-privileged.pdf
+ * CHAPTER 12.4 SV39 RISCV. 
+ * Chpater 14 for Svpmbt extension
+ * Page table entry: N | PBMT | Reserved | PPN[2] = [53:28] | PPN[1] = [27:19] | PPN[0] = [18:10] | RSW | Flags
  */
 struct pte {
     uint64_t flags : 8;
     uint64_t rsw : 2;
-    uint64_t ppn : 44;
-    uint64_t reserved : 7;
-    uint64_t pbmt : 2;
+    uint64_t ppn : 44;                      // 
+    uint64_t reserved : 7;                  //
+    uint64_t pbmt : 2;                      //
     uint64_t n : 1;
 };
+
+/*
+
+
+STUFF TO KNOW! RISC V 12.3-4 PRIVILEGED
+- ASID: Address Space Identifer: see 4.11?? Not sure slides are uncleaer :/
+
+- SATP Register
+    - SATP[43:0]: Holds PPN of root page table (so physical address divide by 4096) So to find address multiply by 4 * 1024
+    - So upper 20 cleared and we can get address by doing (SATP << 20) >> 20 and then taking that result adn doing << 12
+- Leaf versus non-leaf PTE:
+    - Leaf PTE: PPN field gives teh physical page number to the actual physical memor py page. I.e. paddr = (PPN << 12) | page_offset
+        - When oen of R | W | X = 1
+    - Non-leaf PTE: Gives phsycial page number of ANOTHER page table
+        - Page tables are stored in RAM!!!!!!!!!!
+        - Look at teh R, W, X bits. Shoudl all be 0
+
+- Bits 63-39 fetch and store addresses must equal to bit 38 otherwis page-fault exception!
+- However final mapped physical address IS zero extended??
+- 
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
 
 // INTERNAL MACRO DEFINITIONS
 //
 
 #define VPN(vma) ((vma) / PAGE_SIZE)
-#define VPN2(vma) ((VPN(vma) >> (2 * 9)) % PTE_CNT)
+#define VPN2(vma) ((VPN(vma) >> (2 * 9)) % PTE_CNT) // gets vpn2. Essentially shift out the offset. Then find the page index for the page table
 #define VPN1(vma) ((VPN(vma) >> (1 * 9)) % PTE_CNT)
 #define VPN0(vma) ((VPN(vma) >> (0 * 9)) % PTE_CNT)
 
@@ -206,6 +263,12 @@ void memory_init(void) {
     // MEGA_SIZE = 2 MB
     // GIGA_SIZE = 1 GB
 
+
+    /*
+    
+        root level table some can be directly mapping to pages while others do not
+    
+    */
     // Identity mapping of MMIO region as two gigapage mappings
     for (pma = 0; pma < RAM_START_PMA; pma += GIGA_SIZE)
         main_pt2[VPN2(pma)] = leaf_pte((void *)pma, PTE_R | PTE_W | PTE_G);
@@ -262,6 +325,31 @@ void memory_init(void) {
           (heap_end - heap_start) / 1024);
 
     // FIXME: Initialize the free chunk list here
+    /*
+        idk any more; heap is stored in RAM right
+        use kimg_end = kernel image end which is teh stsart of our thing
+    */
+
+
+    // free_chunk_list = new
+    struct page_chunk* head = kmalloc(sizeof(struct page_chunk));  // create the initial page chunk which holds from end of kernel image to end of RAM
+    struct page_chunk* sentinel = kmalloc(sizeof(struct page_chunk));   // create a sentinel
+    if (head == NULL)
+    {
+        kprintf("FAILED TO ALLOCATE THE FIRST PAGE CHUNK!\n");
+        return;
+    }
+    head->pagecnt = ((uintptr_t)RAM_END - (uintptr_t)heap_end) / PAGE_SIZE;        // find total number of pages we can have for user. We use the heap end
+    head->next = NULL;
+    head->address = (uint64_t)heap_end;             // start of chunk
+    sentinel->next = head;
+    sentinel->pagecnt = 0;
+    sentinel->address = 1;  // for testing purposes. 
+    free_chunk_list = sentinel;
+
+
+    // ^
+
 
     // Allow supervisor to access user memory. We could be more precise by only
     // enabling supervisor access to user memory when we are explicitly trying
@@ -310,7 +398,65 @@ mtag_t discard_active_mspace(void) {
 
 void *map_page(uintptr_t vma, void *pp, int rwxug_flags) {
     // FIXME
-    return NULL;
+    // return phsycial pointer as actual address?
+    /*
+        walk along page tables, if it is invalid then we have to allocate/create a new one
+    */
+    if (vma & (PAGE_SIZE - 1))  // trick to see if its algiend or not. If its not aligend what do we do?
+    {
+        // do something not sure yet
+    }
+    
+
+
+
+
+
+    struct pte* pt2_pte = &main_pt2[VPN2(vma)];
+    struct pte* pt1_pte;
+    struct pte* pt0_pte;
+    // check if root entry is valid
+    if (PTE_VALID(*pt2_pte))
+    {
+        pt1_pte = (struct pte*) VPN(pt2_pte->ppn);
+    }
+    else
+    {
+        // exception???
+    }
+    if (PTE_VALID(*pt1_pte))
+    {
+        pt0_pte = (struct pte*) VPN(pt1_pte->ppn);
+    }
+    else
+    {
+        
+        // exception???
+    }
+    /*
+        struct pte {
+        uint64_t flags : 8;
+        uint64_t rsw : 2;
+        uint64_t ppn : 44;                      // 
+        uint64_t reserved : 7;                  //
+        uint64_t pbmt : 2;                      //
+        uint64_t n : 1;
+};
+    
+    */
+    if (PTE_VALID(*pt0_pte))
+    {
+        pt0_pte->flags = rwxug_flags | PTE_V;
+        //pt0_pte->rsw  free for kernel to use
+        pt0_pte->ppn = VPN((uintptr_t) pp); 
+    }
+    else
+    {
+        // exception???
+    }
+
+
+    return (void*) vma;
 }
 
 void *map_range(uintptr_t vma, size_t size, void *pp, int rwxug_flags) {
@@ -342,30 +488,91 @@ int validate_vstr(const char *vs, int rug_flags) {
     // FIXME
     return 0;
 }
+/*
+
+Allocating physicla pages := removing from chunk list??
+
+*/
 
 void *alloc_phys_page(void) {
     // FIXME
-    return NULL;
+    return alloc_phys_pages(1);
 }
 
 void free_phys_page(void *pp) {
     // FIXME
+    free_phys_pages(pp, 1);
     return;
 }
 
 void *alloc_phys_pages(unsigned int cnt) {
     // FIXME
-    return NULL;
+    if (cnt <= 0)
+    {
+        kprintf("ERROR: alloc_phys_pages: Count <= 0: Cnt: %d", cnt);
+        return;
+    }
+    // walk along chunk list
+    struct page_chunk* head = free_chunk_list;
+    struct page_chunk* prev_chunk;  // previous of actaul_chunk
+    struct page_chunk* actual_chunk = NULL; // acutal chunk chosen
+    // sentinel makes it easy to do head/edge cases
+    // start at the sentinel
+    uint32_t min_cnt = UINT32_MAX;  // set as -1
+    while (head->next != NULL)
+    {
+        if (head->next->pagecnt >= cnt && head->next->pagecnt < min_cnt)
+        {
+            prev_chunk = head;
+            actual_chunk = head->next;
+            min_cnt = MIN(min_cnt, cnt);
+        }
+    }
+    if (actual_chunk == NULL)
+    {
+        kprintf("No avaiable pages can be allocated fro cnt: %d", cnt);
+
+        // shoudl panic here but idk how
+        return;
+    }
+    // otherwise head->next is not null and we found
+    // actual start address
+    uint64_t pages_start_address = actual_chunk->next->address;
+    actual_chunk->next->pagecnt -= cnt;
+    if (actual_chunk->pagecnt > 0)
+    {
+        actual_chunk->address += cnt * PAGE_SIZE;
+    }
+    else    // chunk size is not equal
+    {
+        prev_chunk->next = actual_chunk->next;
+        kfree(actual_chunk);
+    }
+    return (uintptr_t) pages_start_address;
 }
 
 void free_phys_pages(void *pp, unsigned int cnt) {
     // FIXME
+    // we do not implement check for double free. Although we can do that later
+    if (cnt <= 0) return;
+    struct page_chunk* freed_chunk = kmalloc(sizeof(struct page_chunk));
+    freed_chunk->pagecnt = cnt;
+    freed_chunk->address = (uint64_t)(uintptr_t) pp;
+    freed_chunk->next = free_chunk_list->next;
+    free_chunk_list->next = freed_chunk;
     return;
 }
 
 unsigned long free_phys_page_count(void) {
     // FIXME
-    return 0;
+    unsigned long cnt = 0;
+    struct page_chunk* head = free_chunk_list->next;
+    while (head != NULL)
+    {
+        cnt += head->pagecnt;
+        head = head->next;
+    }
+    return cnt;
 }
 
 int handle_umode_page_fault(struct trap_frame *tfr, uintptr_t vma) {
