a393c19656449a68b5f50664f6f7a3c6f85bb433
Alex Lin <alexlin3@illinois.edu>
2025-11-27 16:49:37 -0600

    bruh why i tnot owrk

--- sys/main.c
+++ sys/main.c
@@ -23,7 +23,7 @@
 #include "thread.h"
 #include "timer.h"
 
-#define INITEXE "rogue"  // FIXME
+#define INITEXE "zork"  // FIXME
 
 // INITEXE: Controls the kernel program
 
@@ -152,7 +152,7 @@ void run_init(void) {
     struct process *curr = running_thread_process();
     open_file(DEVMNTNAME, "uart1",&curr->uiotab[2]);
     char *argv[] = { INITEXE, "10", NULL };   // 
-    process_exec(initexe, 2, argv);
+    process_exec(initexe, 1, argv);
     // elf_load(initexe, &start_trek);
     // start_adele(uart_dev);
 
