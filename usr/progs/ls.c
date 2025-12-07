b85bd105ff42ea263ecd0dad9e4bd0f3405bfc4f
asmuley2 <asmuley2@illinois.edu>
2025-12-06 22:52:03 -0600

    robust ls implementation

---
+++ usr/progs/ls.c
@@ -0,0 +1,66 @@
+#include "syscall.h"
+#include "string.h"
+
+#define LS_BUFSZ 1024
+
+void main (int argc, char *argv[]){
+
+    char buf[LS_BUFSZ];
+
+    for (int i = 1; i < argc; i++){
+        int fd = _open(-1, argv[i]);
+        if (fd < 0) continue;
+
+        int is_ktfs = 0;
+        if (strcmp(argv[i], "c") == 0) is_ktfs = 1;
+
+        while (1){
+            int br = _read(fd, buf, LS_BUFSZ);
+
+            if (br == 0) break;
+
+            dprintf(STDOUT, "%d", buf);
+
+            if (is_ktfs) dputs(STDOUT, "\r\n"); //because the other listings don't have it built in like ktfs
+        }
+    }
+    dputs(STDOUT, "\n");
+
+
+}
+    
+
+    // for (int i = 1; i < argc; i++){
+    //     int fd = _open(-1, argv[i]);
+    //     if (fd < 0) continue;
+    //
+    //
+    //     if (strcmp(argv[i], "c") == 0) {
+    //         _read(fd, buf, LS_BUFSZ);
+    //         dprintf(STDOUT, "%s", buf);
+    //
+    //         dprintf(STDOUT, "\n");//idk I can't debug this
+    //     }
+    //     else {  //case for / and dev
+    //
+    //         while (1){
+    //             int br = _read(fd, buf, LS_BUFSZ);
+    //             if (bw =< 0) break;
+    //
+    //             dprintf(STDOUT, "%s\n", buf);//ok the idea is to print everything on a new line
+    //                                                  //maybe the way I'm doing it rn is wrong though.
+    //                                                  //u can tune it however u want
+    //
+    //             //note: if the previous impl requires more logic for the return
+    //             //on the last print, just use dputs
+    //         }
+    //
+    //
+    //     }
+    //
+    //     dprintf(STDOUT,"\r"); 
+    }
+
+    
+
+}
