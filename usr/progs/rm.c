a6cd8c92219546c08254381b5e949389e4189b9f
Aditya Jain <adityaj8@illinois.edu>
2025-12-07 20:36:40 -0600

    touch and rm add newline

--- usr/progs/rm.c
+++ usr/progs/rm.c
@@ -4,7 +4,7 @@
 void main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         if (_fsdelete(argv[i]) < 0) {
-            printf("failed to remove %s", argv[i]);
+            printf("failed to remove %s\n", argv[i]);
         }
     }
     _exit();
